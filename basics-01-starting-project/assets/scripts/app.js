be0a5ca7bc43a24a3f054098fe866d5db086ca21
Suhas KS <suhass149@gmail.com>
2020-12-29 21:36:58 +0530

    section 2 chapter 30

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,18 +1,104 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function addTwoNumbers(num1, num2) {
-    let sum = num1 + num1;
-    // alert('add function called; result is '+sum);
-    // return num1 + num2;
-    return sum;
+// function addTwoNumbers(num1, num2) {
+//     let sum = num1 + num1;
+//     // alert('add function called; result is '+sum);
+//     // return num1 + num2;
+//     return sum;
+// }
+
+
+function getParsedNumberInput() {
+    return parseInt(userInput.value);
 }
 
-addTwoNumbers(1, 2);
-addTwoNumbers(3, 4);
-currentResult = addTwoNumbers(5, 5);
+function addTwoNumbers() {
+    const enteredNumber = getParsedNumberInput();
+    const calculationDescription = `${currentResult} + ${enteredNumber}`
+    currentResult = currentResult + enteredNumber;
+    // event listener listens to clicks always, but the outputResult function does not execute again
+    // so if outputResult function is included within the eventlistener call function, then it will be executed every time
+    outputResult(currentResult, calculationDescription);
+}
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+function subtractTwoNumbers() {
+    const enteredNumber = getParsedNumberInput();
+    const calculationDescription = `${currentResult} - ${enteredNumber}`
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult, calculationDescription);
+}
 
-outputResult(currentResult, calculationDescription);
+function multiplyTwoNumbers() {
+    const enteredNumber = getParsedNumberInput();
+    const calculationDescription = `${currentResult} * ${enteredNumber}`
+    currentResult = currentResult * enteredNumber;
+    outputResult(currentResult, calculationDescription);
+}
+
+function divideTwoNumbers() {
+    const enteredNumber = getParsedNumberInput();
+    const calculationDescription = `${currentResult} / ${enteredNumber}`
+    currentResult = currentResult / enteredNumber;
+    outputResult(currentResult, calculationDescription);
+}
+
+
+// function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
+//     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+//     outputResult(currentResult, calcDescription);
+// }
+
+
+// function addTwoNumbers() {
+//     const enteredNumber = getParsedNumberInput();
+//     const initialResult = currentResult;
+//     currentResult = currentResult + enteredNumber;
+//     createAndWriteOutput('+', initialResult, enteredNumber);
+// }
+
+// function subtractTwoNumbers() {
+//     const enteredNumber = getParsedNumberInput();
+//     const initialResult = currentResult;
+//     currentResult = currentResult - enteredNumber;
+//     createAndWriteOutput('-', initialResult, enteredNumber);
+
+// function multiplyTwoNumbers() {
+//     const enteredNumber = getParsedNumberInput();
+//     const initialResult = currentResult;
+//     currentResult = currentResult * enteredNumber;
+//     createAndWriteOutput('*', initialResult, enteredNumber);
+// }
+
+// function divideTwoNumbers() {
+//     const enteredNumber = getParsedNumberInput();
+//     const initialResult = currentResult;
+//     currentResult = currentResult / enteredNumber;
+//     createAndWriteOutput('/', initialResult, enteredNumber);
+// }
+
+
+addBtn.addEventListener('click', addTwoNumbers);
+subtractBtn.addEventListener('click', subtractTwoNumbers);
+multiplyBtn.addEventListener('click', multiplyTwoNumbers);
+divideBtn.addEventListener('click', divideTwoNumbers);
+
+
+
+
+
+
+
+
+
+
+
+// addTwoNumbers(1, 2);
+// addTwoNumbers(3, 4);
+// currentResult = addTwoNumbers(5, 5);
+
+// currentResult = (currentResult + 10) * 3 / 2 - 1;
+// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+addBtn.addEventListener('click', addTwoNumbers);
+
