8a40386b6134d438ad70f3069fea92da8cb4cff8
anonymous-oracle <suhass149@gmail.com>
2021-01-24 10:48:21 +0530

    section 8 chapter 27

--- arrays-01-starting-code/maps.js
+++ arrays-01-starting-code/maps.js
@@ -1,25 +1,29 @@
-let person = { name: 'Max' };
-let person2 = { name: 'Manuel' };
+// let person = { name: 'Max' };
+// let person2 = { name: 'Manuel' };
 
-let personData = new Map([[person, [{ date: 'yesterday', price: 10 }]]]);
-console.log(personData);
-console.log('\n');
-console.log(personData.get(person));
-console.log('\n');
-personData.set(person2, { date: 'two weeks ago', price: 100 })
-console.log(personData);
-console.log('\n');
-for (const [key, value] of personData.entries()){
-    console.log(key, value);
-}
-console.log('\n');
-for (const key of personData.keys()){
-    console.log(key);
-}
-console.log('\n');
-for (const value of personData.values()){
-    console.log(value);
-}
-personData.set({name:'Anonymous'},{date:'2 days ago', price:200})
-console.log('\n');
-console.log(personData);
+// let personData = new Map([[person, [{ date: 'yesterday', price: 10 }]]]);
+// console.log(personData);
+// console.log('\n');
+// console.log(personData.get(person));
+// console.log('\n');
+// personData.set(person2, { date: 'two weeks ago', price: 100 })
+// console.log(personData);
+// console.log('\n');
+// for (const [key, value] of personData.entries()){
+//     console.log(key, value);
+// }
+// console.log('\n');
+// for (const key of personData.keys()){
+//     console.log(key);
+// }
+// console.log('\n');
+// for (const value of personData.values()){
+//     console.log(value);
+// }
+// personData.set({name:'Anonymous'},{date:'2 days ago', price:200})
+// console.log('\n');
+// console.log(personData);
+
+////////////////////////////////////////////////////////////////////
+
+// WEAK MAP
--- arrays-01-starting-code/sets.js
+++ arrays-01-starting-code/sets.js
@@ -1,21 +1,33 @@
-const ids = new Set([1,2,3]);
-console.log(ids);
+// const ids = new Set([1, 2, 3]);
+// console.log(ids);
 
-for(const entry of ids.entries()){
-    console.log(entry);
-}
+// for (const entry of ids.entries()) {
+//     console.log(entry);
+// }
 
-for(const entry of ids.entries()){
-    console.log(entry[0]);
-}
+// for (const entry of ids.entries()) {
+//     console.log(entry[0]);
+// }
 
-ids.add(2);
-console.log(ids);
-ids.add(4);
-console.log(ids);
-ids.delete(3);
-console.log(ids);
+// ids.add(2);
+// console.log(ids);
+// ids.add(4);
+// console.log(ids);
+// ids.delete(3);
+// console.log(ids);
 
 ////////////////////////////////////////////////////////////////////
 
 // WEAK SET
+let person = { name: 'Max' };
+let persons = new WeakSet();
+persons.add(person);
+console.log(persons);
+
+// now suppose after some operations, the 'person' object will be removed by setting it to null, then the garbage collector will free
+// up that memory; however if it were an ordinary set, then the garbage collector won't clear the memory
+
+// ...some operations
+person = null;
+
+console.log(persons);
