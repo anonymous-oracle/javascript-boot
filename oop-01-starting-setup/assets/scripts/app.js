159865ed3d948267d3f597670957910c14e8fd7e
anonymous-oracle <suhass149@gmail.com>
2021-01-27 23:55:33 +0530

    section 10 chapter 11

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,9 +1,33 @@
 class Product {
     title = 'DEFAULT';
     description;
-    price;   
+    price;
+    static init(){
+        console.log(`product class object ${this.title} initialized`);
+    }
+
+    constructor(title, description, price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        
+    }
 }
 
-let product = new Product();
-console.log(product);
+let product = new Product('product 1', 'this is a product', 8.99);
+// console.log(product);
 
+
+class ProductList {
+    products = [product, new Product('product 2', 'this is a product', 7.99)];
+
+    constructor() { };
+
+    render() {
+        this.products.forEach((prod) => { console.log(prod) })
+    };
+}
+
+let productList = new ProductList();
+productList.render();
+console.log()
