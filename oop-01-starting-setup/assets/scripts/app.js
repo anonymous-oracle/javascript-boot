c606ea3979795d0d3bb13f514401e24f07d81c19
anonymous-oracle <suhass149@gmail.com>
2021-01-29 23:04:50 +0530

    section 10 chapter 13

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -2,16 +2,23 @@ class Product {
     title = 'DEFAULT';
     description;
     price;
-    static init(){
-        console.log(`product class object ${this.title} initialized`);
+    static numProducts = 0;
+    static incrementNumProducts() {
+        this.numProducts++;
+    }
+
+    static getNumProducts() {
+        return this.numProducts;
     }
 
     constructor(title, description, price) {
         this.title = title;
         this.description = description;
         this.price = price;
-        
+        Product.incrementNumProducts();
     }
+    // set price(val) {this._price = val;}
+    get price() { return this._price; }
 }
 
 let product = new Product('product 1', 'this is a product', 8.99);
@@ -19,15 +26,36 @@ let product = new Product('product 1', 'this is a product', 8.99);
 
 
 class ProductList {
-    products = [product, new Product('product 2', 'this is a product', 7.99)];
-
-    constructor() { };
+    products = [];
+    totalValue = 0;
+    constructor(...multipleProducts) {
+        this.products = Array.from(multipleProducts);
+        this.products.forEach(product => { this.totalValue += product.getPrice() })
+    };
 
     render() {
         this.products.forEach((prod) => { console.log(prod) })
     };
+
+    addProduct(product) {
+        this.products.push(product);
+        this.totalValue += product.getPrice();
+    }
+
+    addProducts(...multipleProducts) {
+        this.products.push(...multipleProducts);
+        multipleProducts.forEach(product => { this.totalValue += product.getPrice() })
+
+    }
+    get totalListValue() { return this.totalValue; }
 }
 
 let productList = new ProductList();
 productList.render();
-console.log()
+// static methods are exclusive the class object and not the class instance; also static fields can only be accessed within static methods
+productList.addProducts(product, new Product('product 2', 'this is product 2', 5.99),
+    new Product('product 3', 'this is product 3', 35.99), new Product('product 4', 'this is product 4', 45.99), new Product('product 5', 'this is product 5', 52.99));
+
+console.log(`The number of Products is: ${Product.getNumProducts()}`);
+productList.render();
+console.log(`The total list value is ${productList.totalListValue()}`);
