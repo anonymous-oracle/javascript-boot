f73c821546c075f48b16edbf01d990d77457d33e
anonymous-oracle <suhass149@gmail.com>
2021-02-10 23:43:30 +0530

    section 14 chapter 5

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -3,16 +3,20 @@ const button = document.querySelector('button');
 
 // };
 
-const buttonClickHandler = () => { alert('Button was clicked!') };
+const buttonClickHandler = (event) => { console.log(event); };
 const anotherButtonClickHandler = () => {
     console.log('This was clicked!');
 };
 
 // button.onclick = buttonClickHandler;
 // button.onclick = anotherButtonClickHandler;
+const boundFn = buttonClickHandler.bind(this);
 
 button.addEventListener('click', buttonClickHandler);
 
 setTimeout(()=>{
     button.removeEventListener('click', buttonClickHandler);
 }, 2000);
+
+// SUPPOSE WE USED ANONYMOUS FUNCTIONS FOR CALLBACKS, THEN THE REMOVE EVENT LISTENER WON'T WORK
+// BECAUSE THE ANONYMOUS FUNCTIONS GET CREATED EVERYTIME WITH NEW REFERENCE VALUES
